1749d6e0c4142920ce4f681bfb092af42d80f7f9
Trice97 <bolinp@gmail.com>
2025-08-13 19:32:02 +0200

    task-0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,10 +6,10 @@ Module containing index_range function for pagination
 
 def index_range(page: int, page_size: int) -> tuple[int, int]:
     """
-    Return a tuple containing start index and end index 
+    Return a tuple containing start index and end index
     for pagination parameters.
     """
     start = (page - 1) * page_size
     end = page * page_size
-    
+
     return (start, end)
