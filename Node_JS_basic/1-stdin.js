ccb8c5c11ea21ec8d86126bbf8993b426c4576f0
Trice97 <bolinp@gmail.com>
2025-09-05 17:11:49 +0200

    task-1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,21 @@
+/**
+ * Script that displays a welcome message,
+ * reads user input from stdin,
+ * prints the user's name,
+ * and shows a closing message when input ends.
+ */
+
+//that first bloc incites the user to write his name
 process.stdout.write(
    "Welcome to Holberton School, what is your name?\n")
 
+//the second bloc listen to what is typed by the user and store it 
 process.stdin.on("data",(data) => {
     const name =data.toString().trim();
     process.stdout.write(`Your name is: ${name}\n`);
     });
 
+// this bloc display a message regarding the closure of the program
 process.stdin.on("end", () => {
     process.stdout.write("This important software is now closing\n");
 });
