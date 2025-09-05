e5a4173136886a8acaff764696129ef0983bc641
Trice97 <bolinp@gmail.com>
2025-09-05 19:29:58 +0200

    Task-2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -4,13 +4,13 @@ function countStudents(path) {
   try {
     // Read file synchronously and split into lines
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split(/\r?\n/).filter(line => line.trim() !== '');
-    
+    const lines = data.split(/\r?\n/).filter((line) => line.trim() !== '');
+
     // Remove header and process student data
     const students = lines.slice(1);
     const fields = {};
-    
-    students.forEach(line => {
+
+    students.forEach((line) => {
       const [firstname, , , field] = line.split(',');
       if (firstname && field) {
         const cleanField = field.trim();
@@ -19,10 +19,10 @@ function countStudents(path) {
         fields[cleanField].push(cleanFirstname);
       }
     });
-    
+
     // Display results
     console.log(`Number of students: ${students.length}`);
-    Object.keys(fields).sort().forEach(field => {
+    Object.keys(fields).sort().forEach((field) => {
       console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
     });
   } catch (error) {
