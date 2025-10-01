54254cb4fd495b512b34d4a22dd60e6fd0849138
Trice97 <bolinp@gmail.com>
2025-10-01 15:38:29 +0200

    task8.2

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,14 +1,14 @@
 export default class HolbertonClass {
-    constructor (size, location) {
-        this._size = size;
-        this.location = location;
-    }
-
-valueOf() {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;  // Assure-toi que c'est bien _location avec underscore
+  }
+  
+  valueOf() {
     return this._size;
   }
   
   toString() {
-    return this._location;
+    return this._location;  // Assure-toi que c'est bien _location avec underscore
   }
 }
